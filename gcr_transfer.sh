4950abdfa07003242dafa7ab0d0e56d62a8ca1f5
David Li <davidli012345@gmail.com>
2020-12-05 21:12:27 +0000

    updating script

--- gcr_transfer.sh
+++ gcr_transfer.sh
@@ -2,9 +2,10 @@
 # Simple script to repush docker images from one repository to another
 # This is done incremently because my images are massive
 original_image="grandfleet/dolwarp"
-# github packages has a strange way of having images, you can have
+# github packages expects a project and then an image for that project
 new_image="friendlyuser/dimages/lwarp_docker"
-target_acr="gcr.io"
+# Github package registry with a repository - do not use ghcr.io (massive mistake for me)
+target_acr="docker.pkg.github.com"
 temp_file="image_list.txt"
 rm $temp_file
 
@@ -16,25 +17,12 @@ while read -r line; do
       echo "Found line latest"
     else
       # docker image push and delete
-      echo "$line"
       docker pull $original_image:$tag
       docker tag $original_image:$tag $target_acr/$new_image:$tag
       docker push $target_acr/$new_image:$tag
       docker image rm $original_image:$tag
+      docker image rm $target_acr/$new_image:$tag
     fi
 done < "$temp_file"
 
-# variable=$(awk 'BEGIN { ORS="" } { print p"\047"$0"\047"; p=", " } END { print "\n" }' image_list.txt)
-# for i in $variable; do
-#   echo $i
-# done
-exit
-# Old approach with hardcoded numbers
-# for i in v{0.65,0.66,0.67}; do
-#   u="${u:+$u, }$i"
-# done
-# echo "$u"
-
-exit
-docker login
-target_acr="gcr.io"
+rm $temp_file
